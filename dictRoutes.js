a8888c4025ff797c13a66ef0c5ce7442183d4b2f
khalito <khalito@Le-Big-Mac.local>
2019-09-04 17:05:28 +0200

    Cosmetic changes to template names

--- dictRoutes.js
+++ dictRoutes.js
@@ -20,21 +20,24 @@ const routes = (app) => {
     })
     .get((req, res, next) => {
         res.render('index');
-    })
+    });
 
     app.route('/searchResult')
     .get((req, res, next) => {
         //query the database
         next();
-    }, (req, res, next) => {
+    }, (req, res, next) => { // diesen Kram mit einer Methode aus dem Controller ersetzen !
         let word = 'this is a test word';
         let type = 'this is a test type';
         let translation = 'this is a test translation';
+        let id = 'this is a test id';
         res.render('searchResult', {
             'q' : req.query.q,
-            'word' : 'word',
-            'type' : 'type',
-            'translation' : 'translation'});
+            'word' : word,
+            'type' : type,
+            'translation' : translation,
+            '_id' : id
+        });
     });
 
     app.route('/newWord')
